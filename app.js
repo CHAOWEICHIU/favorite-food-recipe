879de4b21da84d5aa620e2f0c96f56a6c10af0e2
CHAO WEI <chaoweichiu@gmail.com>
2016-09-20 15:15:32 -0400

    not ready

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 'use strict'
 require('./api/models/db.js')
-const express = require('express');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const routes = require('./api/routes');
-const path = require('path');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'),
+	  ejs = require('ejs'),
+	  mongoose = require('mongoose'),
+	  routes = require('./api/routes'),
+ 	  path = require('path'),
+	  app = express(),
+	  bodyParser = require('body-parser'),
+	  server = require('http').createServer(app),
+	  io = require('socket.io').listen(server)
 
 
 // Define port to run
 const port = process.env.PORT || 3000;
 
+// Listen for request
+server.listen(port);
+console.log(`listening on port ${port}`)
+
+
+io.sockets.on('connection', (socket)=>{
+	socket.on('send msg', (data)=>{
+		io.sockets.emit('get msg', data)
+		console.log(data)
+	})
+	console.log(socket)
+})
 
 // Add middleware to console.log every request
 // app.use((req,res,next)=>{
 // 	console.log(`${req.method} ${req.url}`);
 // 	next();
 // })
-
-
 
 // Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')))
@@ -40,6 +52,3 @@ app.use('/assets', express.static(__dirname + '/public'))
 app.use('/api', routes);
 
 
-// Listen for request
-app.listen(port);
-console.log(`listening on port ${port}`)
--- public/js/controllers/chat.controller.js
+++ public/js/controllers/chat.controller.js
@@ -1,5 +1,15 @@
 angular.module('myApp').controller('ChatCtrl', ChatCtrl)
 
-function ChatCtrl(){
+function ChatCtrl(socketio){
+	var vm = this;
+
+	vm.message = ''
+	
+	vm.sendMessage = ()=>{
+		console.log(vm.msg)
+	}
+	socketio.on('chat', (msg)=>{
+
+	})
 	console.log('ChatCtrl!')
 }
